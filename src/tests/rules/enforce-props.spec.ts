5a1081822ad2be28aac3df0a1a12220cc7564934
florian.baranya <florian.baranya@pctrade.hu>
2026-02-03 10:22:50 +0100

    setup tests

---
+++ src/tests/rules/enforce-props.spec.ts
@@ -0,0 +1,92 @@
+import { RuleTester } from '@typescript-eslint/rule-tester';
+import tsParser from '@typescript-eslint/parser';
+import { enforceProps } from '../../rules/enforce-props';
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      projectService: {
+        allowDefaultProject: ['*.ts*'],
+      },
+    },
+  },
+});
+
+ruleTester.run('enforce-props', enforceProps, {
+  valid: [
+    {
+      code: `<View><Image testID="test" /></View>`,
+      options: [
+        {
+          componentsToCheck: ['Image', 'TouchableOpacity'],
+          propsToCheck: ['testID'],
+        },
+      ],
+    },
+    {
+      code: `<View><TouchableOpacity testID="test" /></View>`,
+      options: [
+        {
+          componentsToCheck: ['Image', 'TouchableOpacity'],
+          propsToCheck: ['testID'],
+        },
+      ],
+    },
+  ],
+  invalid: [
+    {
+      code: `<View><Image /><TouchableOpacity/></View>`,
+      options: [
+        {
+          componentsToCheck: ['Image'],
+          propsToCheck: ['testID'],
+        },
+      ],
+      errors: [
+        {
+          messageId: 'missingProp',
+          data: {
+            componentName: 'Image',
+            prop: 'testID',
+          },
+        },
+      ],
+    },
+    {
+      code: `<View><Image /><TouchableOpacity testID="test"/></View>`,
+      options: [
+        {
+          componentsToCheck: ['Image', 'TouchableOpacity'],
+          propsToCheck: ['onPress', 'testID'],
+        },
+      ],
+      errors: [
+        {
+          messageId: 'missingProp',
+          data: {
+            componentName: 'Image',
+            prop: 'onPress',
+          },
+        },
+        {
+          messageId: 'missingProp',
+          data: {
+            componentName: 'Image',
+            prop: 'testID',
+          },
+        },
+        {
+          messageId: 'missingProp',
+          data: {
+            componentName: 'TouchableOpacity',
+            prop: 'onPress',
+          },
+        },
+      ],
+    },
+  ],
+});
