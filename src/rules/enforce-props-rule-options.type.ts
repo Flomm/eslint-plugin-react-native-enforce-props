97cbe5fe3ab3fa78ca0f8d93e793056c6eba9aea
florian.baranya <florian.baranya@pctrade.hu>
2026-02-05 09:18:30 +0100

    add presets

--- src/rules/enforce-props-rule-options.type.ts
+++ src/rules/enforce-props-rule-options.type.ts
@@ -1,6 +1,9 @@
-export type EnforcePropsRuleOptions = [
-  {
-    componentsToCheck?: string[];
-    propsToCheck?: string[];
-  }?,
-];
+import { RulePresets } from './presets';
+
+export type EnforcePropsRuleOptions = {
+  componentsToCheck?: string[];
+  propsToCheck?: string[];
+  preset?: keyof typeof RulePresets;
+};
+
+export type EnforceableOptions = Omit<Required<EnforcePropsRuleOptions>, 'preset'>;
